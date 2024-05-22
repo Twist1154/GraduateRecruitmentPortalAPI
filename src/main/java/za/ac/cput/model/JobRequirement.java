f6cd4c8b31b2815c61e71ee885d45d13d114af84
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:37:52 +0200

    feat: Add GraduatePortalDocument.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/JobRequirement.java
+++ src/main/java/za/ac/cput/model/JobRequirement.java
@@ -5,16 +5,29 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.UUID;
 
+/**
+ * Represents a job requirement for a vacancy.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EqualsAndHashCode
 @Entity
+@Table(name = "JobRequirement") // Maps the entity to a table with the same name
 public class JobRequirement {
+
     @Id
-    private String jobRequirementId;
+    @GeneratedValue(strategy = GenerationType.AUTO) // Specifies the ID generation strategy
+    private UUID jobRequirementId;
+
     private String requirement;
-    @ManyToOne(cascade = CascadeType.PERSIST)
+
+    @ManyToOne(cascade = CascadeType.PERSIST) // Specifies the many-to-one relationship with Vacancy
+    @JoinColumn(name = "vacancy_id", nullable = false) // Specifies the join column name
     private Vacancy vacancy;
 }
