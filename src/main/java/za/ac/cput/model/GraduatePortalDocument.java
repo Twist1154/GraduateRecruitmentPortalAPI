8fdc0cf4761ae1283686f3682466a541aa68f784
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:36:52 +0200

    feat: Add GraduatePortalDocument.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/GraduatePortalDocument.java
+++ src/main/java/za/ac/cput/model/GraduatePortalDocument.java
@@ -3,24 +3,39 @@ package za.ac.cput.model;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+/**
+ * Represents a document uploaded to the graduate portal.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-@EqualsAndHashCode
 @Entity
+@Table(name = "GraduatePortalDocument") // Maps the entity to a table with the same name
 public class GraduatePortalDocument {
+
     @Id
-    private String documentId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Specifies the ID generation strategy
+    private Long documentId;
+
     private String documentName;
+
     private String documentType;
+
     private String documentLocation;
+
+    @Lob // Specifies that the documentData field should be stored as a large object in the database
     private byte[] documentData;
+
+    @NotNull
     private LocalDate dateAdded;
 }
