65e8e3491784a2c6c2b05f18ec822309df6dfdea
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:33:26 +0200

    feat: Add Graduate.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/Graduate.java
+++ src/main/java/za/ac/cput/model/Graduate.java
@@ -5,22 +5,23 @@ import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-
 import java.io.Serializable;
 import java.util.Set;
 
 /**
- * @author Chuma Nxazonke
-
- * Date: 24 November 2022
+ * Represents a graduate in the system.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
  */
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-@EqualsAndHashCode(callSuper = false)
 @Entity
+@Table(name = "Graduate") // Maps the entity to a table with the same name
 public class Graduate extends User implements Serializable {
 
     private String preferredName;
@@ -31,13 +32,13 @@ public class Graduate extends User implements Serializable {
 
     private String country;
 
-    @JoinColumn(name = "cvId")
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL) // Specifies the relationship with Cv
+    @JoinColumn(name = "cv_id") // Specifies the join column name
     private Cv cv;
 
-    @OneToMany(mappedBy = "graduate", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "graduate", cascade = CascadeType.ALL) // Specifies the relationship with Qualification
     private Set<Qualification> qualifications;
 
-    @OneToMany(mappedBy = "graduate", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "graduate", cascade = CascadeType.ALL) // Specifies the relationship with Experience
     private Set<Experience> experiences;
 }
