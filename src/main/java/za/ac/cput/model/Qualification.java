40f4c178306b93e62220e157d4e1e36e1ebbb318
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:41:17 +0200

    feat: Add Qualification.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/Qualification.java
+++ src/main/java/za/ac/cput/model/Qualification.java
@@ -5,19 +5,32 @@ import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Objects;
+
+/**
+ * Represents a qualification obtained by a graduate.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "Qualification") // Maps the entity to a table with the same name
 public class Qualification implements Serializable {
+
     @Id
-    private String qualificationId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Specifies the ID generation strategy
+    private Long qualificationId;
+
     private String qualificationName;
+
     private String qualificationDescription;
-    @ManyToOne
-    @JoinColumn(name="user_id", nullable=false)
+
+    @ManyToOne // Specifies the many-to-one relationship with Graduate
+    @JoinColumn(name = "graduate_id", nullable = false) // Specifies the join column name
     private Graduate graduate;
 }
