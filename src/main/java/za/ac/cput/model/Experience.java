c271190262a961a1953aff83de5c0b71c13e5bd3
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:30:59 +0200

    feat: Add exprience entity class with required fields and validations

--- src/main/java/za/ac/cput/model/Experience.java
+++ src/main/java/za/ac/cput/model/Experience.java
@@ -1,27 +1,41 @@
 package za.ac.cput.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Represents the work experience of a graduate.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "Experience") // Maps the entity to a table with the same name
 public class Experience {
+
     @Id
     private String experienceId;
-    private String  jobTitle;
+
+    @NotNull
+    private String jobTitle;
+
     private String assumedRole;
+
+    @NotNull
     private String startDate;
+
     private String endDate;
+
     @ManyToOne
-    @JoinColumn(name="user_id", nullable=false)
-    Graduate graduate;
+    @JoinColumn(name = "user_id", nullable = false)
+    private Graduate graduate;
 
 }
