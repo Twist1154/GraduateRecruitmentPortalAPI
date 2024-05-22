288f70a55dd1d3c3cd5df4cb0a6e2ec6e584008d
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:43:57 +0200

    feat: Add User.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/User.java
+++ src/main/java/za/ac/cput/model/User.java
@@ -1,10 +1,5 @@
 package za.ac.cput.model;
 
-/*
-   User.java
-   Damone Hartnick
- */
-
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -12,20 +7,32 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 /**
- * @author Mbuso Kotobe
- * */
+ * Represents a user in the system.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
 @SuperBuilder(toBuilder = true)
 public abstract class User implements Serializable {
+
     @Id
     protected String userId;
+
     protected String firstName;
+
     protected String surname;
+
     protected String email;
+
     protected String password;
+
     protected String cellphone;
+
     protected String userRole;
 }
