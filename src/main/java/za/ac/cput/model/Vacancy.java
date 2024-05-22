368054f622d9a91132e76327d15c2412c8b27ee5
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:45:33 +0200

    feat: Add Vacancy.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/Vacancy.java
+++ src/main/java/za/ac/cput/model/Vacancy.java
@@ -4,34 +4,50 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.List;
 import java.util.Set;
 
 /**
- * @author Mbuso Kotobe
- * */
+ * Represents a job vacancy in the system.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EqualsAndHashCode
 @Entity
+@Table(name = "Vacancy") // Maps the entity to a table with the same name
 public class Vacancy {
+
     @Id
     private String vacancyId;
+
     private String vacancyTitle;
+
     private String jobType;
+
     private String jobRole;
+
     private boolean isApproved;
+
     private String location;
+
     @Column(length = 1024)
     private String jobDescription;
+
     @Column(length = 1024)
     private String aboutCompany;
-    @OneToMany(mappedBy = "vacancy" , cascade = CascadeType.ALL )
+
+    @OneToMany(mappedBy = "vacancy", cascade = CascadeType.ALL) // Specifies the one-to-many relationship with JobResponsibility
     private Set<JobResponsibility> jobResponsibilities;
-    @OneToMany(mappedBy = "vacancy" , cascade = CascadeType.ALL )
+
+    @OneToMany(mappedBy = "vacancy", cascade = CascadeType.ALL) // Specifies the one-to-many relationship with JobRequirement
     private Set<JobRequirement> jobRequirements;
-    @ManyToOne(cascade = CascadeType.PERSIST)
+
+    @ManyToOne(cascade = CascadeType.PERSIST) // Specifies the many-to-one relationship with Recruiter
+    @JoinColumn(name = "recruiter_id", nullable = false) // Specifies the join column name
     private Recruiter recruiter;
 }
