89856b459e51cf0d9b2483300c32d8dd25c3ba84
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:42:42 +0200

    feat: Add Recruiter.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/Recruiter.java
+++ src/main/java/za/ac/cput/model/Recruiter.java
@@ -1,6 +1,5 @@
 package za.ac.cput.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -10,17 +9,25 @@ import java.time.LocalDate;
 import java.util.Set;
 
 /**
- * @author Mbuso Kotobe
- * */
+ * Represents a recruiter in the system.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-@EqualsAndHashCode(callSuper = true)
 @Entity
+@Table(name = "Recruiter") // Maps the entity to a table with the same name
 public class Recruiter extends User implements Serializable {
+
     private String companyName;
+
     private LocalDate dateAdded;
-    @OneToMany
+
+    @OneToMany(mappedBy = "recruiter", cascade = CascadeType.ALL) // Specifies the one-to-many relationship with Vacancy
     private Set<Vacancy> vacancies;
 }
