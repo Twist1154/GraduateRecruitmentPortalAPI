a13a8ed3142de4104b1da9696b312af3792d7b47
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:07:10 +0200

    Chore: Updated Models class

--- src/main/java/za/ac/cput/model/Department.java
+++ src/main/java/za/ac/cput/model/Department.java
@@ -2,29 +2,38 @@ package za.ac.cput.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.sun.istack.NotNull;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Objects;
 
 /**
- * @author Chadrack Mbuyi
- * */
-@Getter
-@AllArgsConstructor
+ * Department entity class
+ *
+ * Represents a department within a faculty at the university.
+ *
+ * @Author Chadrack Mbuyi
+ */
+@Data
 @NoArgsConstructor
-@Builder
+@AllArgsConstructor
+@SuperBuilder
 @Entity
 public class Department implements Serializable {
 
+    private static final long serialVersionUID = 1L; // Added serialVersionUID
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define ID generation strategy
     private String departmentId;
 
+    @NotNull // Mark departmentName as not null
     private String departmentName;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // Set fetch type to LAZY
+    @JoinColumn(name = "faculty_id") // Specify join column
     private Faculty faculty;
 }
