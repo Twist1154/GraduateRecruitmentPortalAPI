1a9d5948d4947c8bcd4e362f7d99a9d7a0c93dc0
Twist1154n <rethabile1154n@gmail.com>
2024-05-23 01:39:56 +0200

    feat: Add JobResponsibility.java entity class with required fields and validations

--- src/main/java/za/ac/cput/model/JobResponsibility.java
+++ src/main/java/za/ac/cput/model/JobResponsibility.java
@@ -5,16 +5,29 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.UUID;
 
+/**
+ * Represents a job responsibility for a vacancy.
+ *
+ * Author: Rethabile Ntsekhe
+ * Student number: 220455430
+ */
+
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EqualsAndHashCode
 @Entity
+@Table(name = "JobResponsibility") // Maps the entity to a table with the same name
 public class JobResponsibility {
+
     @Id
-    private String jobResponsibilityId;
+    @GeneratedValue(strategy = GenerationType.AUTO) // Specifies the ID generation strategy
+    private UUID jobResponsibilityId;
+
     private String responsibility;
-    @ManyToOne(cascade = CascadeType.PERSIST)
+
+    @ManyToOne(cascade = CascadeType.PERSIST) // Specifies the many-to-one relationship with Vacancy
+    @JoinColumn(name = "vacancy_id", nullable = false) // Specifies the join column name
     private Vacancy vacancy;
 }
